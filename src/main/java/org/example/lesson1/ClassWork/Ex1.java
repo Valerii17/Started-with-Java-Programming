c1c66ae3097d794740a658a41880dbddd6097a34
Valerii <vershininvv70@gmail.com>
2023-06-09 15:50:45 +0700

    Inition commit

---
+++ src/main/java/org/example/lesson1/ClassWork/Ex1.java
@@ -0,0 +1,7 @@
+package org.example.lesson1.ClassWork;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/lesson1/HomeWork/Hw1.java
@@ -0,0 +1,7 @@
+package org.example.lesson1.HomeWork;
+
+public class Hw1 {
+    public static void main(String[] args) {
+
+    }
+}
