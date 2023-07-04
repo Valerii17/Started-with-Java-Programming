83bcd479066b6c250794c3d21e6a274eb7145c8f
Valerii <vershininvv70@gmail.com>
2023-07-04 23:08:07 +0700

    OOP1 HomeWork 1

--- src/main/java/org/example/oop1/homeWork/Hwoop1.java
+++ src/main/java/org/example/oop1/homeWork/Hwoop1.java
@@ -2,16 +2,25 @@ package org.example.oop1.homeWork;
 
 public class Hwoop1 {
     public static void main(String[] args) {
-        // Создание категорий
+
         Category category1 = new Category("Продукты питания");
         Category category2 = new Category("Бытовая техника");
 
-        // Создание товаров и добавление их в категории
+
         Product product1 = new Product("Хлеб", 1.5, 4);
         Product product2 = new Product("Молоко", 2.3, 5);
         Product product3 = new Product("Телевизор", 500, 4);
         Product product4 = new Product("Кофеварка", 100, 2);
         Product product5 = new Product("Мороженное", 5, 3);
+        /**
+         * @apiNote 1)Создать класс Товар, имеющий переменные имя, цена, рейтинг.
+         * 2)Создать класс Категория, имеющий переменные имя и массив товаров. Создать несколько объектов класса Категория.
+         * 3)Создать класс Basket, содержащий массив купленных товаров.
+         * 4)Создать класс User, содержащий логин, пароль и объект класса Basket. Создать несколько объектов класса User.
+         * 5)Вывести на консоль каталог продуктов. (все продукты магазина)
+         * 6)Вывести на консоль покупки посетителей магазина. (После покупки у пользователя добавляется товар,
+         * а из магазина - удаляется)
+         */
 
         category1.addProduct(product1);
         category1.addProduct(product2);
@@ -19,18 +28,18 @@ public class Hwoop1 {
         category2.addProduct(product4);
         category1.addProduct(product5);
 
-        // Создание пользователей
+
         User user1 = new User("user1", "password1");
         User user2 = new User("user2", "password2");
 
-        // Добавление товаров в корзину пользователей
+
         user1.getBasket().addProduct(product1);
         user1.getBasket().addProduct(product3);
         user2.getBasket().addProduct(product2);
         user2.getBasket().addProduct(product5);
 
 
-        // Удаление товаров из каталога, которые пользователь добавил в корзину
+
         for (Product product : user1.getBasket().getProducts()) {
             category1.removeProduct(product);
             category2.removeProduct(product);
@@ -40,7 +49,7 @@ public class Hwoop1 {
             category2.removeProduct(product);
         }
 
-        // Вывод каталога продуктов
+
         System.out.println("Каталог продуктов:");
         System.out.println("Категория: " + category1.getName());
         for (Product product : category1.getProducts()) {
@@ -51,7 +60,7 @@ public class Hwoop1 {
             System.out.println("Название: " + product.getName() + ", Цена: " + product.getPrice() + ", Рейтинг: " + product.getRating());
         }
 
-        // Вывод покупок пользователей
+
         System.out.println("Покупки пользователей:");
         System.out.println("Пользователь " + user1.getLogin() + ":");
         for (Product product : user1.getBasket().getProducts()) {
